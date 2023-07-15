c44c3e4d6c84712ccaf5596e66ef343c0537d5d8
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-07-15 16:28:00 +0300

    update the addcard function

--- main.js
+++ main.js
@@ -12,26 +12,41 @@ btnAdd.addEventListener('click', function() {
 document.querySelector('#cancel').addEventListener('click', () => { cardAdder.classList.add('hide') });
 
 document.querySelector('#save').addEventListener('click', function() {
- let question = document.querySelector('#question');
- let answer = document.querySelector('#answer');
- 
-cardBord.appendChild(createCard(question,answer));
+  let question = document.querySelector('#question').value;
+  let answer = document.querySelector('#answer').value;
+
+  cardBord.appendChild(createCard(question, answer));
 
 })
 
+
+
+
 function createCard(question, answer) {
-  
-  const innerCard = `    
-          <div class="card my-3 px-0">
-          <div class="card-header text-end  border-bottom border-danger"><a href="" class="btn">cancel</a></div>
+
+  const innerCard = document.createElement('div');
+  innerCard.classList.add('card');
+  innerCard.classList.add('my-3');
+  innerCard.classList.add('px-0');
+  innerCard.classList.add('cards')
+
+  innerCard.innerHTML = `
+        <div class="card-header text-end  border-bottom border-danger">
+         <a href="" class="btn">cancel</a></div>
           <div class="card-body">
-            <h5 class="card-title text-start">${question} ?</h5>
+            <h5 class="card-title text-start">${question}?</h5>
             <div class="card-text">
-              <p class="text-center py-4 answer hide">${answer}</p>
+              <p class="text-center py-4 answer show">${answer}</p>
             </div>
           </div>
-        </div>
 `
-return innerCard;
-}
 
+innerCard.addEventListener('click', function(){
+      innerCard.querySelector('p').classList.remove('show');
+       });
+
+
+
+
+  return innerCard;
+}
